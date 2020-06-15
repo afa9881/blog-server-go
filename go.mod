2b581f85e4d48b8c694503695a5dbf7d04bfaf48
afa8991 <w240559665@gmail.com>
2020-06-15 21:52:08 +0800

    init

---
+++ go.mod
@@ -0,0 +1,11 @@
+module blog-server-go
+
+go 1.14
+
+require (
+	github.com/astaxie/beego v1.12.1
+	github.com/gin-gonic/gin v1.6.3
+	github.com/go-ini/ini v1.57.0
+	github.com/jinzhu/gorm v1.9.13
+	github.com/unknwon/com v1.0.1
+)
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"blog-server-go/pkg/setting"
+	"blog-server-go/routers"
+)
+
+func main() {
+	router := routers.InitRouter()
+
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+		Handler:        router,
+		ReadTimeout:    setting.ReadTimeout,
+		WriteTimeout:   setting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	s.ListenAndServe()
+}
